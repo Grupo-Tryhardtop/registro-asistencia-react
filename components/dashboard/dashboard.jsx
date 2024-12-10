8708707aa2e7d8cf270ab2cd95509f8bbc233d01
OrianH <0ri4n771@gmail.com>
2024-12-10 13:47:09 -0400

    mi rama db

---
+++ components/dashboard/dashboard.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import './panel.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+const Dashboard = () => {
+    const [isMobile, setIsMobile] = useState(window.matchMedia("(max-width: 990px)").matches);
+    const [isNavVisible, setNavVisible] = useState(false);
+    const [isCompact, setCompact] = useState(false);
+
+    const toggleMenu = () => {
+        if (isMobile) {
+            setNavVisible(!isNavVisible);
+        } else {
+            setCompact(!isCompact);
+        }
+    };
+
+    const handleLogout = (event) => {
+        event.preventDefault();
+        localStorage.removeItem('userSession');
+        window.location.href = 'login.html';
+    };
+
+    return (
+        <div className={`dashboard ${isCompact ? 'dashboard-compact' : ''}`}>
+            <div className={`dashboard-nav ${isNavVisible ? 'mobile-show' : ''}`}>
+                <header>
+                    <a className="menu-toggle" onClick={toggleMenu}>
+                        <i className="fas fa-bars"></i>
+                    </a>
+                    <a className="brand-logo">
+                        <i className="fas fa-anchor"></i>
+                        <h2>PANEL</h2>
+                    </a>
+                </header>
+                <nav className="dashboard-nav-list">
+                    <a href="#" className="dashboard-nav-item active">
+                        <i className="fas fa-tachometer-alt"></i> dashboard
+                    </a>
+                    <div className="nav-item-divider"></div>
+                    <a href="login.html" id="logout" className="dashboard-nav-item" onClick={handleLogout}>
+                        <i className="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </nav>
+            </div>
+            <div className='dashboard-app'>
+                <header className='dashboard-toolbar'>
+                    <a href="#!" className="menu-toggle" onClick={toggleMenu}>
+                        <i className="fas fa-bars"></i>
+                    </a>
+                </header>
+                <div className='dashboard-content'>
+                    <div className='container'>
+                        <div className='card'>
+                            <div className='card-header'>
+                                <h1>¡Bienvenido de vuelta!</h1>
+                            </div>
+                            <div className='card-body'>
+                                <p>  
+                                    ¡Bienvenido de vuelta!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Footer from '../components/footer/footer.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Dashboard from '../components/dashboard/dashboard.jsx'
+// import Footer from '../components/footer/footer.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <Footer />
+    <Dashboard/>
+    
   </StrictMode>,
 )
